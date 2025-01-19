755203508722a2401f350a33e563d7bd4dcb3672
magi2004 <magiabalans22it@srishakthi.ac.in>
2025-01-19 22:59:27 +0530

    Projects updated

--- portf/src/components/Projects.jsx
+++ portf/src/components/Projects.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import ProjectImage from '../assets/logo.jpg'
 
 const projects = [
     {
         title: 'Portfolio',
         technology: 'MERN Stack, Tailwind CSS',
         description: 'This is a project description',
-        image: 'https://via.placeholder.com/150'
+        image: ProjectImage
     },
     {
         title: 'Canteen management system',
         technology: 'MERN Stack, Tailwind CSS',
         description: 'This is a project description',
-        image: 'https://via.placeholder.com/150'
+        image: ProjectImage
     },
     {
         title: 'TBA',
         technology: 'MERN Stack, Tailwind CSS',
         description: 'This is a project description',
-        image: 'https://via.placeholder.com/150'
+        image: ProjectImage
     }
 ]
 
@@ -28,12 +29,12 @@ const Projects = () => {
             <h2 className='text-4xl text-center font-bold mb-12 bg-gradient-to-tr from-blue-700 to-cyan-500 rounded-full py-3'>Projects</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
             {projects.map(projects => (
-                <div key={projects.id} className='bg-gradient-to-tr from-blue-700 to-cyan-500 p-8 rounded-xl transform transition-transform duration-200 hover:scale-105'>
+                <div key={projects.id} className='bg-gradient-to-tr from-blue-700 to-cyan-500 p-8 rounded-xl transform transition-transform duration-100 hover:scale-110'>
                     <h3 className='text-2xl font-bold mb-4'>{projects.title}</h3>
                     <p>{projects.description}</p>
                     <p>Technologies used: {projects.technology}</p>
-                    <img src={projects.image} alt='' className='w-48 h-48 rounded-full object-cover'/>
-                    <a href='#' className='text-yellow-400 hover:text-yellow-300'>Learn More</a>
+                    <img src={projects.image} alt='' className='w-48 h-48 rounded-3xl m-3 object-cover'/>
+                    <a href='#' className='inline-block px-4 py-2 rounded-full bg-gradient-to-bl from-blue-700 to-cyan-500 text-orange-400 font-bold hover:text-white'>Github</a>
                 </div>
 
             ))}
